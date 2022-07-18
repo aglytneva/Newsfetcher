0cff5efa391f7f7308c8e7e8fc816d88a6529869
Anastasiya Lytneva <kvartira-62@yandex.ru>
2022-07-18 23:17:27 +0300

    Создана новая ветка

--- app/src/main/java/com/example/newsfetcher/feature/data/Mapper.kt
+++ app/src/main/java/com/example/newsfetcher/feature/data/Mapper.kt
@@ -4,6 +4,7 @@ import com.example.newsfetcher.feature.data.model.ArticleRemoteModel
 import com.example.newsfetcher.feature.domain.ArticleModel
 
 fun ArticleRemoteModel.toDomain ()= ArticleModel (
+
     author = author ?:"",
     title =title ?:"",
     description =description ?:"" ,
